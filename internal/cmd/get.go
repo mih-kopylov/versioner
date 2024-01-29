80d3fdb981e89182f353eb1af0741c6ee34a36d9
Mikhail Kopylov <mih.kopylov@yandex.ru>
2024-01-29 09:12:48 +0300

    Fix `release` option for `get` command

--- internal/cmd/get.go
+++ internal/cmd/get.go
@@ -17,26 +17,40 @@ func NewGetCmd(parent *cobra.Command, config *app.Config) *cobra.Command {
 		Long: `Print current version.
 
 For the current version 1.2.3-SNAPSTHOT:
-- versioner get major -- prints 1-SNAPSHOT
+- versioner get major           -- prints 1-SNAPSHOT
 - versioner get major --release -- prints 1
-- versioner get minor -- prints 1.2-SNAPSHOT
+- versioner get minor           -- prints 1.2-SNAPSHOT
 - versioner get minor --release -- prints 1.2
-- versioner get patch -- prints 1.2.3-SNAPSHOT
+- versioner get patch           -- prints 1.2.3-SNAPSHOT
 - versioner get patch --release -- prints 1.2.3
+- versioner get                 -- prints 1.2.3-SNAPSHOT -- same for the "patch" mode
+- versioner get --release       -- prints 1.2.3 -- same for the "patch" mode
 
 If used with 'major' argument then '1.2.3' becomes just '1'.
 If used with 'minor' argument then '1.2.3' becomes just '1.2'.
 Optional suffix is kept in both cases.
 `,
-		Args: cobra.ExactArgs(1),
+		Args: cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			currentVersion, err := fileops.GetVersion(config)
 			if err != nil {
 				return err
 			}
 
-			result, err := versionops.HandleMode(
-				currentVersion, args[0], versionops.RemoveMinor,
+			result := currentVersion
+			if releaseOnly {
+				result, err = versionops.Release(result)
+				if err != nil {
+					return err
+				}
+			}
+
+			modeString := "patch"
+			if len(args) > 0 {
+				modeString = args[0]
+			}
+			result, err = versionops.HandleMode(
+				result, modeString, versionops.RemoveMinor,
 				versionops.RemovePatch, func(s string) (string, error) {
 					return s, nil
 				},
@@ -45,19 +59,12 @@ Optional suffix is kept in both cases.
 				return err
 			}
 
-			if releaseOnly {
-				result, err = versionops.Release(result)
-				if err != nil {
-					return err
-				}
-			}
-
 			fmt.Println(result)
 
 			return nil
 		},
 	}
-	parent.Flags().BoolVar(&releaseOnly, "release", false, "If used, version suffix is trimmed")
+	command.Flags().BoolVar(&releaseOnly, "release", false, "If used, version suffix is trimmed")
 	parent.AddCommand(command)
 	return command
 }
